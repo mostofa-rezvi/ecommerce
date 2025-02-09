83ee81baa63ebe22214f0bf5d40b258bbefd759e
mostofa-rezvi <mostofa.aminur.rezvi@gmail.com>
2025-02-09 16:15:16 +0600

    First Day

---
+++ ecommerce/apps/EcommerceSite/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FaConfig, FaIconComponent, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font_awesome_icons';
+
+@Component({
+  standalone: true,
+  imports: [RouterModule, FaIconComponent],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent implements OnInit{
+  // title = 'EcommerceSite';
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faConfig.defaultPrefix = 'far';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+}
