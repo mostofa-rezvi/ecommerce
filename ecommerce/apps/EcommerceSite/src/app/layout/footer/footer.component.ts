c6ffc045ce980b06ddb64c6d8abb30b5c696b116
mostofa-rezvi <mostofa.aminur.rezvi@gmail.com>
2025-07-06 21:32:14 +0600

    Fix: Font Awesome Icons

--- ecommerce/apps/EcommerceSite/src/app/layout/footer/footer.component.ts
+++ ecommerce/apps/EcommerceSite/src/app/layout/footer/footer.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import {
+  FontAwesomeModule,
+  FaIconLibrary,
+} from '@fortawesome/angular-fontawesome';
+import {
+  faFacebookF,
+  faTwitter,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons';
+import { faTruckFast } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faFacebookF, faTwitter, faYoutube, faTruckFast);
+  }
+}
